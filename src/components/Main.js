c3cf4306a4a6b89433453f3148533aed65c1b89e
VelinP <dottallot1@hotmail.com>
2023-04-22 00:22:46 +0300

    Added spinner to home page that works with state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,15 @@
 import * as request from '../services/requestservice.js'
 import { useEffect , useState } from 'react'
 import { HomeComp } from './Homecompo.js'
-
+import { Spinner } from './Spinner.js'
 
 export const Main = () =>{
     const [users, setUsers] = useState([])
-    
+    const [boolspinner,setboolspinner] = useState(true)
+
     useEffect(()=> 
     {
-        request.get("http://localhost:4000").then((info)=> setUsers(info))
-    },[])
+        request.get("http://localhost:4000").then((info)=> {setUsers(info);setboolspinner(false)})},[])
             
 
   
@@ -19,6 +19,8 @@ export const Main = () =>{
       <div className="maincontentdiv">
         
         <ul className='mainuls'>
+        {boolspinner && <Spinner/>}
+
         {users?.length >0 ? users.map(item => <HomeComp email={item.email} key ={item._id}/>) : "No content 😢"}
         </ul>
       </div>
