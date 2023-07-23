c16f295aa31c661f8e6317545620d347f7c2a7b4
VelinP <dottallot1@hotmail.com>
2023-07-24 01:04:35 +0300

    updated css

--- src/components/AlloffersRow.js
+++ src/components/AlloffersRow.js
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom"
 
 export const AlloffersRow = (props) =>{
-    console.log(props.info)
     
     return(
         <div className="alloffersrowmaindiv">
                 
-        <Link to={`/offers/${props.info._id}/details`} ><img  src={props.info.picture} alt="quest" /></Link>
-         
-         <div className="alloffersrowdetailsdiv">
-            <h4>Offer: {props.info.offername} </h4>
-            <p>Price: {props.info.price}</p>
-        </div>
+            <Link to={`/offers/${props.info._id}/details`} ><img  src={props.info.picture} alt="quest" /></Link>
+            
+            <div className="alloffersrowdetailsdiv">
+                <h4>Offer: {props.info.offername} </h4>
+                <p>Price: {props.info.price}</p>
+            </div>
          </div>
      )
 
