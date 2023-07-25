661596edf12cee7dd051560ba01b4f0f1cea7c97
VelinP <dottallot1@hotmail.com>
2023-07-25 03:30:57 +0300

    updated profile layout

--- src/components/ProfileCards.js
+++ src/components/ProfileCards.js
@@ -14,10 +14,37 @@ export const ProfileCards = () =>{
     useEffect(()=> {request.get(`http://localhost:4000/profile/${useremail}`).then((data)=> {setCards(data);setboolspinner(false)})},[useremail])
     
     return(
+        <>
+        
+        {
+        
+        usercards?.offers?.length > 0 
+        ?
         <div className="profilegriddiv">
-        {boolspinner && <Spinner/>}
-        {usercards.offers?.map(card => <AlloffersRow key={card._id} info={card}/>)}
+            {boolspinner && <Spinner/>}
+            {usercards?.offers?.map(card => <AlloffersRow key={card._id} info={card}/>)}
         </div>
+
+        :
+        <div className='alloffersnoffers'> <h1>User has not made any offers yet</h1> </div>
+
+        }
+        
+        </>
+        
     )
 }
 
+// {offers?.alloffers?.length > 0 ? 
+//     <div className="profilegriddiv">
+        
+//     {offers?.alloffers?.map(card => <AlloffersRow info = {card}key ={card._id}/>) }
+//     </div>
+//     :
+//     <div className='alloffersnoffers'> <h1>No offers currently</h1> </div>
+//     }
+
+// <div className="profilegriddiv">
+//         {boolspinner && <Spinner/>}
+//         {usercards?.offers?.map(card => <AlloffersRow key={card._id} info={card}/>)}
+//         </div>
